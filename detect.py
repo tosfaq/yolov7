7a3dd3264751da58d45c8461b24adeafb2a593e6
Istam Rakhimov <vupmud@gmail.com>
2024-02-13 15:24:23 +0300

    debug print for plot_one_box

--- detect.py
+++ detect.py
@@ -126,6 +126,7 @@ def detect(save_img=False):
 
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
+                        print('XYXY', xyxy)
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
 
             # Print time (inference + NMS)
