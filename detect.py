4806acaba0493e7da3d4df17943e059628efad56
Istam Rakhimov <vupmud@gmail.com>
2024-02-25 05:24:12 +0300

    fixed single color for detect

--- detect.py
+++ detect.py
@@ -59,6 +59,8 @@ def detect(save_img=False):
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    if len(names) == 1:
+        colors = [[80, 80, 80]]
 
     # Run inference
     if device.type != 'cpu':
