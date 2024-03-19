a0d66ad2d5240c661783ef96c4bc956e88d0b23c
Istam Rakhimov <vupmud@gmail.com>
2024-03-19 10:38:17 +0300

    fixed unique

--- test.py
+++ test.py
@@ -293,7 +293,7 @@ def test(data,
     for k, slices in stats_series_dict.items():
         correct, confs, pcls, tcls = [np.concatenate(x, 0) for x in zip(*slices)]
         if len(tcls):
-            for ci, cls in enumerate(tcls.unique()):
+            for ci, cls in enumerate(np.unique(tcls)):
                 pi = (cls == pcls).nonzero(as_tuple=False).view(-1)  # prediction indices
                 if pi.shape[0]:
                     correct_predictions = correct[pi].sum(1).nonzero(as_tuple=False)
